d2802551dc9524439ac7a681cf4672fe253fc5bb
biel-aguma-cosa <49573010+biel-aguma-cosa@users.noreply.github.com>
2026-02-25 19:52:29 -0300

    Update ba.py

--- babbababa/ba.py
+++ babbababa/ba.py
@@ -1,17 +1,22 @@
+import os
 import socket
 import threading
 
 client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-client.connect(('10.12.156.183',55555))
+client.connect((input('HOST IP:'),55555))
 
+DATA = ''
 def recv():
     while True:
         data = client.recv(1024).decode()
         if data:
-            print(data)
+            DATA += '\n'+data
+            os.run('cls')
+            print(DATA)
 thread = threading.Thread(target=recv)
 thread.start()
 
 while True:
     data = input()
+
     client.send(data.encode())
